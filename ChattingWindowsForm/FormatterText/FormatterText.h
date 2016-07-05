11ea8413bfadb18fa0e5ccd2fab5148b629159c2
Bá Hảo Nguyễn <bahao247@gmail.com>
2016-07-06 06:41:19 +0700

    Version 1: Create Project

---
+++ ChattingWindowsForm/FormatterText/FormatterText.h
@@ -0,0 +1,26 @@
+// FormatterText.h
+
+#pragma once
+
+using namespace System;
+using namespace System;
+using namespace System::ComponentModel;
+using namespace System::Collections;
+using namespace System::Windows::Forms;
+using namespace System::Data;
+using namespace System::Drawing;
+
+namespace FormatterText {
+
+	public ref class StructChat
+	{
+		// TODO: Add your methods for this class here.
+	public:
+		String* textChat;
+		System::Drawing::Color color;
+		System::Drawing::Font fontChu;
+
+	public:
+
+	};
+}
